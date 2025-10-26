855628b76268f16e2d0d6d26bd1341c91c6f3734
TheQuantumSushi <signup.trashemail@proton.me>
2025-10-26 21:47:46 +0100

    feat: added emulation functions for all BTB-based gates (except not, which isn't implemented yet)

--- src/tests/asm_tests.py
+++ src/tests/asm_tests.py
@@ -113,3 +113,92 @@ def emulate_asm_not(in1, debug=False):
     emulator.logger.log(f"Output value: {result}")
 
     return result
+
+def emulate_asm_btb_assign(in1, debug=False):
+    code = get_asm_btb_assign(in1)
+    loader = AsmLoader(code)
+    emulator = MuWMEmulator(name='btb_assign', loader=loader, debug=debug)
+
+    # Set input and output addresses of assign gate
+    input_address = emulator.data_start_addr
+    emulator.uc.reg_write(UC_X86_REG_R14, input_address)
+    output_address = emulator.data_start_addr + emulator.cache.line_size  # makes sure output goes in different cache set than input
+    emulator.uc.reg_write(UC_X86_REG_R15, output_address)
+
+    emulator.emulate()
+
+    emulator.logger.log(f"Output value: {emulator.cache.is_cached(output_address)}")
+    
+    # Optional : print BTB statistics to verify training/misprediction
+    emulator.btb.print_stats()
+
+    return emulator.cache.is_cached(output_address)
+
+def emulate_asm_btb_or(in1, in2, debug=False):
+    code = get_asm_exception_btb_or(in1, in2)
+    loader = AsmLoader(code)
+    emulator = MuWMEmulator(name='btb_or', loader=loader, debug=debug)
+
+    emulator.logger.log(f"Starting emulation of BTB-OR({in1}, {in2})...")
+
+    # Set input and output addresses of OR gate
+    input1_address = emulator.data_start_addr
+    emulator.uc.reg_write(UC_X86_REG_R13, input1_address)
+    input2_address = emulator.data_start_addr + emulator.cache.line_size
+    emulator.uc.reg_write(UC_X86_REG_R14, input2_address)
+    output_address = emulator.data_start_addr + 2 * emulator.cache.line_size
+    emulator.uc.reg_write(UC_X86_REG_R15, output_address)
+
+    emulator.emulate()
+
+    emulator.logger.log(f"Output value: {emulator.cache.is_cached(output_address)}")
+
+    return emulator.cache.is_cached(output_address)
+
+def emulate_asm_btb_and(in1, in2, debug=False):
+    code = get_asm_exception_btb_and(in1, in2)
+    loader = AsmLoader(code)
+    emulator = MuWMEmulator(name='btb_and', loader=loader, debug=debug)
+
+    emulator.logger.log(f"Starting emulation of BTB-AND({in1}, {in2})...")
+
+    # Set input and output addresses of AND gate
+    input1_address = emulator.data_start_addr
+    emulator.uc.reg_write(UC_X86_REG_R13, input1_address)
+    input2_address = emulator.data_start_addr + emulator.cache.line_size
+    emulator.uc.reg_write(UC_X86_REG_R14, input2_address)
+    output_address = emulator.data_start_addr + 2 * emulator.cache.line_size
+    emulator.uc.reg_write(UC_X86_REG_R15, output_address)
+
+    emulator.emulate()
+
+    emulator.logger.log(f"Output value: {emulator.cache.is_cached(output_address)}")
+
+    return emulator.cache.is_cached(output_address)
+
+def emulate_asm_btb_and_or(in1, in2, in3, debug=False):
+    code = get_asm_exception_btb_and_or(in1, in2, in3)
+    loader = AsmLoader(code)
+    emulator = MuWMEmulator(name='btb_and_or', loader=loader, debug=debug)
+
+    emulator.logger.log(f"Starting emulation of BTB-AND-OR({in1}, {in2}, {in3})...")
+
+    # Set input and output addresses
+    input1_address = emulator.data_start_addr
+    emulator.uc.reg_write(UC_X86_REG_R13, input1_address)
+    
+    input2_address = emulator.data_start_addr + emulator.cache.line_size
+    emulator.uc.reg_write(UC_X86_REG_R14, input2_address)
+    
+    input3_address = emulator.data_start_addr + 2 * emulator.cache.line_size
+    emulator.uc.reg_write(UC_X86_REG_R12, input3_address)
+    
+    output_address = emulator.data_start_addr + 3 * emulator.cache.line_size
+    emulator.uc.reg_write(UC_X86_REG_R15, output_address)
+
+    emulator.emulate()
+
+    result = emulator.cache.is_cached(output_address)
+    emulator.logger.log(f"Output value: {result}")
+
+    return result
